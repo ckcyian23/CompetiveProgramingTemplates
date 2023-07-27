578221142da2da25460386f5409a217353308406
ckcyi <an18983855326@163.com>
2023-07-27 19:39:20 +0800

    pam回文划分dp示例

--- PAM.cpp
+++ PAM.cpp
@@ -1,3 +1,6 @@
+#include <bits/stdc++.h>
+using namespace std;
+
 struct PAM  {
     static constexpr int M = 26;
     const int n;
@@ -16,7 +19,7 @@ struct PAM  {
     int extend(int p, int c) {
         s[++cur] = c;
         int now = getfail(p);
-        if(!t[now][c]) {
+        if (!t[now][c]) {
             int u = cnt++;
             len[u] = len[now] + 2;
             link[u] = t[getfail(link[now])][c];
@@ -30,8 +33,30 @@ struct PAM  {
     }
 
     int getfail(int x) {
-        while(s[cur - len[x] - 1] != s[cur]) x = link[x];
+        while (s[cur - len[x] - 1] != s[cur]) x = link[x];
         return x;
     }
 
 };
+
+int main() {
+    string s = "aaa";
+    int n = s.size();
+
+    PAM pam(n);
+    int p = 1;
+    vector<int> g(n + 2), dp(n + 1);
+    dp[0] = 1;
+    for (int i = 0; i < n; i++) {
+        p = pam.extend(p, s[i] - 'a');
+        for (int x = p; pam.len[x]; x = pam.slink[x]) {
+            g[x] = dp[i + 1 - pam.len[pam.slink[x]] - pam.diff[x]] ;
+            if (pam.diff[x] == pam.diff[pam.link[x]]) {
+                g[x] = (g[x] + g[pam.link[x]]);
+            }
+            dp[i + 1] += g[x];
+        }
+    }
+
+    cout << dp[n] << '\n';
+}
