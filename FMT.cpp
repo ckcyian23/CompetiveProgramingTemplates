ac2ac19fd8e31dba2e887fe05febeb19f62ec5ee
ckcyi <an18983855326@163.com>
2023-08-08 02:55:50 +0800

    fmt fwt

--- FMT.cpp
+++ FMT.cpp
@@ -1,5 +1,6 @@
+//xor
 template<class T>
-void fmt(vector<T> &a) {
+void fwt(vector<T> &a) {
     int n = a.size();
     for (int k = 1; k < n; k *= 2) {
         for (int i = 0; i < n; i += 2 * k) {
@@ -7,16 +8,39 @@ void fmt(vector<T> &a) {
                 int u = a[i + j], v = a[i + j + k];
                 a[i + j] = u + v;
                 a[i + j + k] = u - v;
-            }
+            } 
         }
     }
 }
 
 template<class T>
-void ifmt(vector<T> &a) {
+void ifwt(vector<T> &a) {
     int n = a.size();
-    fmt(a);
+    fwt(a);
     for (int i = 0; i < n; i++) {
         a[i] /= n;
+    }
+}
+
+//or
+template<class T>
+void fmt(vector<T> &a) {
+    int n = a.size();
+    for (int k = 1; k < n; k *= 2) {
+        for (int i = 0; i < n; i += 2 * k) {
+            for (int j = 0; j < k; j++) {
+                a[i + j + k] += a[i + j];
+            }
+        }
+    }
+}
+void ifmt(vector<T> &a) {
+    int n = a.size();
+    for (int k = 1; k < n; k *= 2) {
+        for (int i = 0; i < n; i += 2 * k) {
+            for (int j = 0; j < k; j++) {
+                a[i + j + k] -= a[i + j];
+            }
+        }
     }
 }
