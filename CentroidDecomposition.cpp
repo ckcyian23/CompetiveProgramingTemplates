26db697b86dcdf17c4037a89ce6617aaecb64ceb
ckcyi <an18983855326@163.com>
2023-08-22 14:55:18 +0800

    点分树/点分治

--- CentroidDecomposition.cpp
+++ CentroidDecomposition.cpp
@@ -1,78 +1,50 @@
-#include <bits/stdc++.h>
-using namespace std;
+//初始化 crt(重心根)， tot
+int crt = -1, tot = n;
 
-using i64 = long long;
-
-
-int main() {
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
-
-
-    int n;
-    cin >> n;
-
-    vector<vector<int>> adj(n);
-    for (int i = 0; i < n - 1; i++) {
-        int u, v;
-        cin >> u >> v;
-        u--, v--;
-        adj[u].push_back(v);
-        adj[v].push_back(u);
+vector<int> vis(n), siz(n);
+function<bool(int, int)> getRt = [&](int u, int p) -> bool {
+    int wgt = 0;
+    siz[u] = 1;
+    for (auto v : adj[u]) {
+        if (vis[v] || v == p) continue;
+        if (getRt(v, u)) return true;
+        siz[u] += siz[v];
+        wgt = max(wgt, siz[v]);
     }
+    wgt = max(tot - siz[u], wgt);
 
-    //初始化 rt， tot
-    int rt = -1, tot = n;
+    if (wgt <= tot / 2) {
+        if (p != -1) siz[p] = tot - siz[u]; //对于重心w，我们分治的是他所联通的各个部分，需要维护tot
+        //tot获取由w所直接连向的点获取，所以得到w后，如果p存在，就改正大小并由(1)直接退出。
+        crt = u;
+        return true;
+    }
+    return false;
+};
 
-    vector<int> vis(n), siz(n);
-    function<void(int, int)> getRt = [&](int u, int p) {
-        int wgt = 0;
-        siz[u] = 1;
-        for (auto v : adj[u]) {
-            if (vis[v] || v == p) continue;
-            getRt(v, u);
-            if (rt != -1) return; //(1)找到就立即退出，因为改过重心p的大小
-            siz[u] += siz[v];
-            wgt = max(wgt, siz[v]);
-        }
-        wgt = max(tot - siz[u], wgt);
+int ans = 0;
+function<void(int)> calc = [&](int u) -> void {
 
-        if (wgt <= tot / 2) {
-            if (p != -1) siz[p] = tot - siz[u]; //对于重心w，我们分治的是他所联通的各个部分，需要维护tot
-            //tot获取由w所直接连向的点获取，所以得到w后，如果p存在，就改正大小并由(1)直接退出。
-            rt = u;
-        }
-    };
+};
 
+//点分树可以在这里存储结构和要维护信息
+auto solve = [&](auto &self, int u) -> void {
+    //calc计算过当前节点的贡献
+    //一般是 ans += calc(u) :容斥思想
+    vis[u] = 1;
+    for (auto v : adj[u]) {
+        if (vis[v]) continue;
 
-
-
-    int ans = 0;
-    function<void(int)> calc = [&](int u) -> void {
-        
-    };
-
-    auto solve = [&](auto &self, int u) -> void {
-        //calc计算过当前节点的贡献
-        //一般是 ans += calc(u) :容斥思想
-        vis[u] = 1;
-        for (auto v : adj[u]) {
-            if (vis[v]) continue;
-
-            //ans -= calc(v)
+        //ans -= calc(v)
 
 
-            //重置 rt 和 tot， 找根并且继续递归
-            rt = -1;
-            tot = siz[v];
-            getRt(v, -1);
-            self(self, rt);
-        }
-    };
-    //先找重心再对重心计算
-    getRt(0, -1);
-    solve(solve, rt);
-
-
-    return 0;
-}
+        //重置 crt 和 tot， 找根并且继续递归
+        crt = -1;
+        tot = siz[v];
+        getRt(v, -1);
+        self(self, crt);
+    }
+};
+//先找重心再对重心计算
+getRt(0, -1);
+solve(solve, crt);
