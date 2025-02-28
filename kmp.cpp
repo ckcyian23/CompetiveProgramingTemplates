e4f9723b8f67fd1547c4acaeca1157df224443a8
ckcyi <an18983855326@163.com>
2025-02-28 14:30:28 +0800

    kmp修改

--- kmp.cpp
+++ kmp.cpp
@@ -1,16 +1,16 @@
 
-vector<int> next(n);
+vector<int> next(n + 1);
 for(int i = 1, j = 0; i < s.size(); i++) {
-    while(j && s[i] != s[j]) j = next[j - 1];
+    while(j && s[i] != s[j]) j = next[j];
     if(s[j] == s[i]) j++;
-    next[i] = j;
+    next[i + 1] = j;
 }
 
 for(int i = 0, j = 0; i < m; i++) {
-    while(j && ss[i] != s[j]) j = next[j - 1];
+    while(j && ss[i] != s[j]) j = next[j];
     if(ss[i] == s[j]) j++;
     if(j == n) {
         cout << i - n + 1 << ' ';
-        j = next[j - 1];
+        j = next[j];
     }
 }
